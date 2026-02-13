202b5d6312afdd15a8a6b7248f8511fb66974ebe
Xinwei Zhu <easonzhu@byu.edu>
2026-02-12 21:42:49 -0700

    v3.6

--- src/app.jsx
+++ src/app.jsx
@@ -1,47 +1,58 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { NewVote } from './newVote/newVote';
+import { History } from './history/history';
+import { Info } from './info/info';
 
 export default function App() {
   return (
-    <div className="body bg-white text-dark">
-      <header className="container-fluid">
-        <nav className="navbar fixed-top navbar-dark bg-primary">
-          <a className="navbar-brand" href="#">
-            GroupVote
-          </a>
-          <menu className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" href="#">
-                New Vote
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                History
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Info
-              </a>
-            </li>
-          </menu>
-        </nav>
-      </header>
+    <BrowserRouter>
+      <div className="body bg-white text-dark">
+        <header className="container-fluid">
+          <nav className="navbar fixed-top navbar-dark bg-primary">
+            <div className="navbar-brand">GroupVote</div>
+            <menu className="navbar-nav">
+              <li className="nav-item">
+                <NavLink end className={({ isActive }) => `nav-link${isActive ? ' active' : ''}`} to="/">
+                  New Vote
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className={({ isActive }) => `nav-link${isActive ? ' active' : ''}`} to="/history">
+                  History
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className={({ isActive }) => `nav-link${isActive ? ' active' : ''}`} to="/info">
+                  Info
+                </NavLink>
+              </li>
+            </menu>
+          </nav>
+        </header>
 
-      <main className="container-fluid">
-        <div className="container py-4">App components go here</div>
-      </main>
+        <Routes>
+          <Route path="/" element={<NewVote />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/info" element={<Info />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
 
-      <footer className="bg-primary text-white">
-        <div className="container-fluid">
-          <span className="text-reset">Author: Xinwei Zhu</span>
-          <a className="text-reset" href="https://github.com/EasonZhuu/startup">
-            GitHub Repository
-          </a>
-        </div>
-      </footer>
-    </div>
+        <footer className="bg-primary text-white">
+          <div className="container-fluid">
+            <span className="text-reset">Author: Xinwei Zhu</span>
+            <a className="text-reset" href="https://github.com/EasonZhuu/startup">
+              GitHub Repository
+            </a>
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Page not found.</main>;
 }

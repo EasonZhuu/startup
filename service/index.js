65bf44f4595502704deb16c4f03a3435c10a6f20
Xinwei Zhu <easonzhu@byu.edu>
2026-03-12 00:52:06 -0600

    implement auth endpoints with cookie token sessions

--- service/index.js
+++ service/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
 
 const app = express();
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
@@ -13,6 +14,52 @@ app.use(express.static('public'));
 
 app.get('/api/health', (req, res) => {
   res.send({ status: 'ok' });
+});
+
+app.post('/api/auth', async (req, res) => {
+  const { email, password } = req.body || {};
+  if (!email || !password) {
+    return res.status(400).send({ msg: 'Missing email or password' });
+  }
+
+  if (getUser('email', email)) {
+    return res.status(409).send({ msg: 'Existing user' });
+  }
+
+  const user = await createUser(email, password);
+  setAuthCookie(res, user);
+  return res.send({ email: user.email });
+});
+
+app.put('/api/auth', async (req, res) => {
+  const { email, password } = req.body || {};
+  const user = getUser('email', email);
+  if (!user || !(await bcrypt.compare(password || '', user.password))) {
+    return res.status(401).send({ msg: 'Unauthorized' });
+  }
+
+  setAuthCookie(res, user);
+  return res.send({ email: user.email });
+});
+
+app.delete('/api/auth', (req, res) => {
+  const token = req.cookies.auth;
+  const user = getUser('token', token);
+  if (user) {
+    clearAuthCookie(res, user);
+  }
+
+  return res.send({});
+});
+
+app.get('/api/user/me', (req, res) => {
+  const token = req.cookies.auth;
+  const user = getUser('token', token);
+  if (!user) {
+    return res.status(401).send({ msg: 'Unauthorized' });
+  }
+
+  return res.send({ email: user.email });
 });
 
 async function createUser(email, password) {
@@ -28,6 +75,20 @@ function getUser(field, value) {
   }
 
   return users.find((user) => user[field] === value);
+}
+
+function setAuthCookie(res, user) {
+  user.token = uuid.v4();
+  res.cookie('auth', user.token, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+function clearAuthCookie(res, user) {
+  delete user.token;
+  res.clearCookie('auth');
 }
 
 app.listen(port, () => {
